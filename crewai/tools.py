a0ef3a832841cff4d0a39df9a4146ca1a182ff48
bikky <bikkydahal@gmail.com>
2024-05-27 22:35:43 +0545

    Minor change

--- crewai/tools.py
+++ crewai/tools.py
@@ -2,6 +2,6 @@ from crewai_tools import YoutubeChannelSearchTool
 import os
 from crewai_tools import SerperDevTool
 
-os.environ["SERPER_API_KEY"] = "34210352cdc15566cdb255f37206de4db4c5e226f97b7fc96910b8eadcaefe97"
+os.environ["SERPER_API_KEY"] = "key"
 
 yt_tool = SerperDevTool()
